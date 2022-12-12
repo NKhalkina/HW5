c462fef976a7a359323cc1c807d8d07daf59bf79
Khalkina <tashik2010@mail.ru>
2022-12-12 17:05:29 +0300

    Решение задачи №38

---
+++ exe38/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+
+//Задаем массив вещеcтвенных чисел
+double[] GetArray(int length)
+{
+    double[] arr = new double[length];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = 5 - rnd.Next(11) + rnd.NextDouble();
+    }
+    return arr;
+}
+
+
+//Выводим на печать
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+// Находим мах и min
+(double, double) GetMinAndMax(double[] array)
+{
+    double max = 0;
+    double min = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+            min = array[i];
+        else if (array[i] > max)
+            max = array[i];
+    }
+    return (min, max);
+}
+
+
+
+Console.WriteLine("Введите размер массива");
+double [] array = GetArray(int.Parse(Console.ReadLine()));
+PrintArray(array);
+(double min, double max) = GetMinAndMax(array);
+
+Console.WriteLine($"Min: {min}, Max: {max}");
+
+Console.WriteLine(max-min);
